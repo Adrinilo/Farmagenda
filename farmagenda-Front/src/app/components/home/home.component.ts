a4c6c8c019822687e6f9fe17ce99d228c7aedf98
Adrián <93315833+Adrinilo@users.noreply.github.com>
2024-06-03 19:14:25 +0200

    Home component

--- farmagenda-Front/src/app/components/home/home.component.ts
+++ farmagenda-Front/src/app/components/home/home.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  admin!: String;
 
+  constructor(private snackBar: MatSnackBar) {    const adminString = localStorage.getItem('admin');
+  if (adminString) {
+    this.admin = JSON.parse(adminString).nombre;
+  }}
+
+  openSnackBar(message: string) {
+    this.snackBar.open(message, 'Cerrar', {
+      duration: 5000, // Duración fija de 5 segundos
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+    });
+  }
+
+  onClickAjustes() {
+    this.openSnackBar('Apartado no disponible');
+  }
 }
